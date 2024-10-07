de29aa79b90afcfd72cefaafffcd017f4c1d7ae2
krishnaTech6 <krishna060702@gmail.com>
2024-10-07 17:50:25 +0530

    search in rotated array binary search

---
+++ binarySearch/SearchInRoatetedArray.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+
+
+int pivotElement(int arr[], int n){
+    int s = 0, e = n -1 ;
+
+    while(s<=e){
+        int mid = s + (e-s)/2;
+        if(arr[mid]>arr[mid-1]&& arr[mid]>arr[mid+1]) {return mid+1;}
+        else if (arr[mid]>arr[mid-1]) {s = mid+1;}
+        else {e = mid-1;}
+    }
+    return -1;
+}
+int binarySearch(int arr[], int k, int s, int e){
+    
+    int mid = s + (e-s)/2;
+
+    while(s<=e){
+        
+        if(arr[mid]==k) return mid;
+        else if (arr[mid]<k) s = mid+1;
+        else e = mid-1;
+
+        mid = s + (e-s)/2;
+
+    }
+    return -1;
+}
+ 
+int main()
+{
+
+    int arr[] = {5,6,7,1,2,3};
+    int n = 6;
+    int pivotIndex = pivotElement(arr, n);
+    int k = 7;
+
+    if(k>= arr[pivotIndex] && k<= arr[n-1]){
+     cout <<binarySearch(arr, k,pivotIndex, n-1 );
+    }else{
+        cout<<binarySearch(arr, k, 0 , pivotIndex-1);
+    }
+    cout<<endl;
+ 
+    return 0;
+}
