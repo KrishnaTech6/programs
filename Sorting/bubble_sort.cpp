bf9f15d9405fd4e60cb94b1da379f0eb6bc76348
krishnaTech6 <krishna060702@gmail.com>
2024-10-12 22:49:30 +0530

    bubble sort 2

--- Sorting/bubble_sort.cpp
+++ Sorting/bubble_sort.cpp
@@ -13,14 +13,23 @@ int main()
     int arr[]= {6,2,4,8,10};
     int n = sizeof(arr)/sizeof(arr[0]);
 
-    for (int i =1 ; i<n;i++ ){  
-        // 0 until n
-        for (int j =0 ; j<n-i; j++){  // 0 until n-i
+    // for (int i =1 ; i<n;i++ ){  
+    //     // 1 until n
+    //     for (int j =0 ; j<n-i; j++){  // 0 until n-i
+    //         if(arr[j]>arr[j+1]){
+    //             swap(arr[j], arr[j+1]);
+    //         }
+    //     }
+    
+    // }
+
+    for(int i =0 ; i<n-1; i++ ){ //0 until n-1
+        for (int j = 0 ; j<n-i-1; j++){ // 0 until n-i-1
             if(arr[j]>arr[j+1]){
                 swap(arr[j], arr[j+1]);
             }
         }
-    
+
     }
 
     printArray(arr, n);
